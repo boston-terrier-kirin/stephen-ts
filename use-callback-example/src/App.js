5bd59d539fd0f45f49eb0fe8170f648f63a12a57
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-10 13:06:13 +0900

    useCallback攻略中

--- use-callback-example/src/App.js
+++ use-callback-example/src/App.js
@@ -1,35 +1,58 @@
 import { useCallback, useEffect, useState } from 'react';
 
+/**
+ * https://javascript.plainenglish.io/5-useeffect-infinite-loop-patterns-2dc9d45a253f
+ */
 function App() {
-  const [message, setMessage] = useState('initial value');
   const [count, setCount] = useState(0);
 
-  const outputLog = (value) => {
-    console.log(value);
+  const outputLog = () => {
+    console.log('Hello');
   };
 
-  useEffect(() => {
-    outputLog(message);
-  }, [outputLog]);
-
-  // const outputLog = useCallback((value) => {
-  //   console.log(value);
-  // }, []);
-
+  /**
+   * これは無限ループしない。
+   * outputLogは毎回変わるけど、変わってuseEffectが呼ばれて、outputLogが呼ばれるだけ
+   */
   // useEffect(() => {
-  //   outputLog(message);
-  // }, [outputLog, message]);
+  //   outputLog();
+  // }, [outputLog]);
+
+  /**
+   * これは無限ループする。
+   * outputLogが変わる⇒setCountでcountが変わる⇒re-render⇒
+   * outputLogが変わる⇒setCountでcountが変わる⇒re-render⇒
+   */
+  // useEffect(() => {
+  //   outputLog();
+  //   setCount(count + 1);
+  // }, [outputLog]);
+
+  /**
+   * useCallbackが効いて、無限ループしない。
+   */
+  const outputLogV2 = useCallback(() => {
+    console.log('HelloV2');
+  }, []);
+
+  useEffect(() => {
+    outputLogV2();
+    setCount(count + 1);
+  }, [outputLogV2]);
 
   return (
     <>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
       <button onClick={() => setCount(count + 1)}>click me</button>
     </>
   );
 }
 
 export default App;
+
+// const outputLog = useCallback((value) => {
+//   console.log(value);
+// }, []);
+
+// useEffect(() => {
+//   outputLog(message);
+// }, [outputLog, message]);
