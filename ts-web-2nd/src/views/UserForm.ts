3fb8c30288cf992690178b18d718a5b6edf83013
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-17 07:16:45 +0900

    ts-web-2nd/Section13-193

---
+++ ts-web-2nd/src/views/UserForm.ts
@@ -0,0 +1,30 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonCkick,
+    };
+  }
+
+  onButtonCkick(): void {
+    console.log('Hello');
+  }
+
+  template(): string {
+    return `
+    <div>
+      <h1>User Form</h1>
+      <input />
+      <button>Click Me</button>
+    </div>
+    `;
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
+  }
+}
