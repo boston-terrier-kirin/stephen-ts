cff2dccd07b7ca8dd8b3aaed01bec02258f24d8b
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-17 14:42:08 +0900

    ts-web-2nd/Section13-212

--- ts-web-2nd/src/index.ts
+++ ts-web-2nd/src/index.ts
@@ -1,12 +1,30 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+// ■UserEdit
+// import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
 
-const user = User.buildUser({ name: 'Steph', age: 35 });
-const root = document.getElementById('root');
+// const user = User.buildUser({ name: 'Steph', age: 35 });
+// const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
+// }
 
-  console.log(userEdit);
-}
+// ■CollectionView
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+
+const users = new Collection('http://localhost:3001/users', (json: UserProps) =>
+  User.buildUser(json)
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    const userList = new UserList(root, users);
+    userList.render();
+  }
+});
+
+users.fetch();
