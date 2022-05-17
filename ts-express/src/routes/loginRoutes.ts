c70f57368cf3d73d4d84fbc0d6fee879cad04509
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-17 16:39:42 +0900

    ts-express/Section14-224

--- ts-express/src/routes/loginRoutes.ts
+++ ts-express/src/routes/loginRoutes.ts
@@ -25,9 +25,35 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email.toUpperCase() + password);
+  if (email && password && email === 'test@test.com' && password === '11111') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password.');
   }
+});
+
+router.get('/', (req, res) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+        <div>
+            <div>You are logged in.</div>
+            <a href="/logout">Logout</a>
+        </div>
+      `);
+  } else {
+    res.send(`
+        <div>
+            <div>You are NOT logged in.</div>
+            <a href="/login">Login</a>
+        </div>
+      `);
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.session = undefined;
+  res.redirect('/');
 });
 
 export { router };
